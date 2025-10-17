2f409db70d6772b4beb04c605ca1ed88361ec2cc
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-10-17 17:37:42 -0300

    ♻refactor: add id validation for user deletion and detaild error loggin

--- src/controllers/DeleteUserController.ts
+++ src/controllers/DeleteUserController.ts
@@ -6,17 +6,24 @@ export class DeleteUserController{
         
         try {
             const { id } = request.params as { id: string };
+
+             if (!id || typeof id !== "string") {
+                return reply.status(400).send({ error: "ID inválido ou ausente" });
+            }
             
             const userService = new DeleteUserService();
-            userService.execute({ id });
+            await userService.execute({ id });
             
            return reply.send("Usuário deletado com sucesso");  
         } catch (error) {
+            console.error("Erro detalhado na exclusão de usuário:", error);
             if (error instanceof Error) {
                 if (error.message.includes("obrigátorio") || error.message.includes("string válida")) {
                     return reply.status(400).send({ error: error.message });
                 } else if (error.message.includes("não encontrado")) {
                     return reply.status(404).send({ error: error.message });
+                } else if (error ) {
+                    
                 }
             }
 
