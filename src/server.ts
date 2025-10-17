1ee105521e5b79e7f96713e3e96ab0f52f673f11
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-10-17 17:41:54 -0300

    ♻refactor: adjust log messages and correctly import userRoutes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify';
-import { routes } from './routes/routes';
+import { userRoutes } from '../src/routes/userRoutes';
 import cors from '@fastify/cors'
 
 
@@ -9,12 +9,14 @@ const app = fastify({ logger: true });
 const start = async () => {
 
     await app.register(cors);
-    await app.register(routes);
+    await app.register(userRoutes);
 
     try {
-        await app.listen({port: 3333})
+        await app.listen({ port: 3333 })
+        return console.log("Api rodando com sucesso na port 3333✅")
     } catch (err) {
         process.exit(1)
+        return console.log("Falha ao rodar Api❌")
     }
 }
 
