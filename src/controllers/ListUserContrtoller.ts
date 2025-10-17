5bb44a63c3f76b50873bacf9dd5bad32565a1c94
Agatha Adrielle Pinto Dantas Santos <20221059@sarah.br>
2025-10-17 17:49:51 -0300

    ✨feat: add endpoint to fetch user by ID

--- src/controllers/ListUserContrtoller.ts
+++ src/controllers/ListUserContrtoller.ts
@@ -1,12 +1,24 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { ListUserService } from "../services/ListUserService";
+import { GetUserByIdService } from "../services/GetUserByIdService";
 
 export class ListUserController{
-    async handle(request: FastifyRequest, reply: FastifyReply) {
+    async listAll(request: FastifyRequest, reply: FastifyReply) {
         
         const listUserService = new ListUserService();
         const users = await listUserService.execute();
 
         reply.send(users);
+    }
+
+    async getById(request:FastifyRequest, reply:FastifyReply) {
+        const { id } = request.params as { id: string };
+        const user = await new GetUserByIdService().execute(id);
+
+        if (!user) {
+            return reply.status(404).send({error: "Usuário não encontrado"})
+        }
+
+        return reply.send(user);
     }
 }
